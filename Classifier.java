5bb6d56c43cb2a54a3897116b616661df25681f9
U-Fatty\Jeffrey <Jeffrey@Fatty.(none)>
2015-11-19 21:35:18 -0800

    first commit

---
+++ Classifier.java
@@ -0,0 +1,24 @@
+/**
+ * @(#)Classifier.java
+ *
+ *
+ * @author 
+ * @version 1.00 2015/11/19
+ */
+
+public class Classifier {
+        
+    /**
+     * Creates a new instance of <code>Classifier</code>.
+     */
+    public Classifier() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("yes");
+    }
+}
